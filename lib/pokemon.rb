c31b6b5f1e5df737a2da0f97c16b753e402ee73e
github username <jeffreyhwang90@gmail.com>
2018-05-01 16:41:56 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,6 +1,6 @@
 class Pokemon
 
-  attr_accessor :name, :type, :db, :id
+  attr_accessor :name, :type, :db, :id, :hp
   @@all = []
 
   def initialize(name)
@@ -9,28 +9,29 @@ class Pokemon
     # @db = db
   end
 
-
+  def alter_hp(hp, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", hp, self.id)
+  end
 
   def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    a = db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    a
   end
 
   def self.find(id, db)
     sql = "SELECT * FROM pokemon WHERE id=(?)"
     poke_array_of_arrays = db.execute(sql, id)
-    pokemon = Pokemon.new(poke_array_of_arrays[0][1])
-    pokemon.type = poke_array_of_arrays[0][2]
-    pokemon.id = poke_array_of_arrays[0][0]
-    pokemon
+    self.converter(poke_array_of_arrays)
   end
 
-  # def self.converter(array)
-  #   pokemon_array = poke_array_of_arrays[0]
-  #   pokemon = Pokemon.new(pokemon_array[1])
-  #   pokemon.type = pokemon_array[2]
-  #   pokemon.id = pokemon_array[0]
-  #   pokemon
-  # end
+  def self.converter(array)
+    pokemon_array = array[0]
+    pokemon = Pokemon.new(pokemon_array[1])
+    pokemon.type = pokemon_array[2]
+    pokemon.id = pokemon_array[0]
+    pokemon.hp = pokemon_array[3]
+    pokemon
+  end
 
 
 
