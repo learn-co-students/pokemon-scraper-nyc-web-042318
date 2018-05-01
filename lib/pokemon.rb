dd3f14f9d1460d101bfc7f52aa1a006f074daba2
Mendy <mendywoly@gmail.com>
2018-05-01 16:32:23 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,17 +1,12 @@
 require 'pry'
-
+# require_relative '../db/alter_table_migration.sql'
 class Pokemon
 
-  attr_accessor :name, :type, :db, :id
+  attr_accessor :name, :type, :db, :id, :hp
 
-  @@all = []
 
   def initialize(name)
     @name = name
-    @type = type
-    @db = db
-    @@all << self
-    @id = id
   end
 
 
@@ -22,15 +17,21 @@ class Pokemon
   def self.find(id, db)
     str ="SELECT * FROM pokemon WHERE id=?"
     response = db.execute(str, id)
-    self.response_to_obj(response)
+    row_to_obj = self.response_to_obj(response)
+    row_to_obj
+  end
+
+  def alter_hp(hp, db)
+      db.execute("UPDATE pokemon SET hp=? WHERE id=? ",hp,self.id)
   end
 
   def self.response_to_obj(array)
+
     new_pokemon = Pokemon.new(array[0][1])
     new_pokemon.id = array[0][0]
     new_pokemon.type = array[0][2]
+    new_pokemon.hp = array[0][3]
     new_pokemon
   end
-
 
 end
